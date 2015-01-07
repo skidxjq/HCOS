a0e9bd2c6e2f2bd6bc10c59c368155b4a0682d5f
futantan <luckytantanfu@gmail.com>
2015-01-07 09:12:41 +0800

    add CheckUserPassword.py

---
+++ CheckUserPassword.py
@@ -0,0 +1,14 @@
+import xml.dom.minidom
+
+
+def check(userName, password):
+    domTree = xml.dom.minidom.parse("users.xml")
+    collection = domTree.documentElement
+    users = collection.getElementsByTagName("user")
+
+    for user in users:
+        userNameGot = user.getElementsByTagName('userName')[0].childNodes[0].data
+        passwordGot = user.getElementsByTagName('passWord')[0].childNodes[0].data
+        if userName == userNameGot and str(password) == passwordGot:
+            return True
+    return False
